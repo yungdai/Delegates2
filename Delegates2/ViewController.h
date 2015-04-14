8bdeb365a111ee42d1c4e9528c1e4ea8ede8d6ed
Yung Dai <yungchidai@gmail.com>
2015-04-14 10:21:30 -0400

    Initial Commit

---
+++ Delegates2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Delegates2
+//
+//  Created by Yung Dai on 2015-04-14.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
